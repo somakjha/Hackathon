ca734c29d5b654402fc809eeaca884a6cae465c6
somakjha <somak.jha@gmail.com>
2022-07-10 16:59:40 +0530

    yo

--- project/src/test/java/pages/HealthInsurancePage.java
+++ project/src/test/java/pages/HealthInsurancePage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import java.util.*;
+
+import org.apache.commons.compress.harmony.pack200.BHSDCodec;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,130 +11,113 @@ import org.openqa.selenium.support.ui.Select;
 import handler.BrowserHandler;
 
 public class HealthInsurancePage {
-	
+
 	public static WebDriver driver;
 	public static String[] data = null;
-	
-	By healthInsuranceIcon = By.xpath("//div[@class='shadowHandlerBox']//i[@class='icon-bg homeIconsBg health-insurance']");
-	By gender = By.className("gender female");
+	public static Select select = null;
+	By gender = By.xpath("//i[@class='gender female']");
 	By fullName = By.id("fullname");
 	By phone = By.id("pmobile");
-	By continueBtn = By.id("continuecta1");
-	By insurePerson = By.xpath("//i[@class='gender self']");
-	By selectAge = By.id("eldestMemberAge"); // this must be passed in select class for selecting drop down for ages.
-	By continueToInsure = By.id("continuecta2");
+	By continueBtn = By.xpath("//button[@type='submit']");
+	By selectAge = By.xpath("//select[@id='Self']");
 	By city = By.id("txtcity");
-	By continueToCity = By.id("continuecta3");
 	By noDiseaseCheckbox = By.id("ped_last");
 	By anyDiseaseCheckBox = By.id("ped_2");
 	By viewPlan = By.id("viewplans");
-	
-	By firstPlan = By.xpath("//span[normalize-space()='NCB Super Premium']");
-	By secondPlan = By.xpath("//span[normalize-space()='Max Saver']");
-	By thirdPlan = By.xpath("//span[normalize-space()='Care']");
-	
+	By submit = By.id("submitButton");
+	By selectCityPin = By.xpath("//li[@class='active']");
+	By cityCross = By.xpath("//div[@class='close_city_box']");
+	By planName = By.xpath("//div//span[@class='quotes_rvmp_card__content__plan_header__name']");
+	List<WebElement> planNameElements = null;
+
 	public HealthInsurancePage(WebDriver driver) {
 		HealthInsurancePage.driver = driver;
 	}
-	
+
 	public void openPolicyBazaar() {
 		BrowserHandler bh = new BrowserHandler();
 		bh.launchApplication(driver);
 	}
-	
+
 	public String openHealthInsurancePage() {
-		driver.findElement(healthInsuranceIcon).click();
+		driver.get("https://health.policybazaar.com/?iscompareexp=1&utm_content=home_v12_compareexp&new=1");
 		String title = driver.getTitle();
-		return title; // here use assert in test class as done in car insurance
-		// and after doing that delete this comment
+		return title;
 	}
-	
+
 	public void selectGender() {
 		driver.findElement(gender).click();
 	}
-	
+
 	public void writeName() {
-		driver.findElement(fullName).sendKeys("Rani");
+		driver.findElement(fullName).sendKeys("dummy");
 	}
-	
+
 	public void writeNumber() {
-		driver.findElement(phone).sendKeys("888888888");
+		driver.findElement(phone).sendKeys("6391876378");
 	}
-	
+
 	public void checkContinueForDetails() {
 		driver.findElement(continueBtn).click();
 	}
-	
-	public void selectInsuringPerson() { // Me
-		driver.findElement(insurePerson).click();
-	}
-	
+
 	public void selectYoungestAge() { // select 18
-		WebElement age = driver.findElement(selectAge);
-		Select selectAge = new Select(age);
-		selectAge.selectByVisibleText("22 yr");
+		select = new Select(driver.findElement(selectAge));
+		select.selectByVisibleText("18 yr");
 	}
-	
+
 	public void selectEldestAge() { // select 100
-		WebElement age = driver.findElement(selectAge);
-		Select selectAge = new Select(age);
-		selectAge.selectByVisibleText("100 yr");
+		select = new Select(driver.findElement(selectAge));
+		select.selectByVisibleText("100 yr");
 	}
-	
-	public void checkContinueForAge() {
-		driver.findElement(continueToInsure).click();
-	}
-	
+
 	public void checkCityBoxFromCityName() {
 		driver.findElement(city).sendKeys("Hyderabad");
 	}
-	
+
 	public void checkCityBoxFromCityPinCode() {
 		driver.findElement(city).sendKeys("500092");
 	}
-	
-	public void checkContinueForCity() {
-		driver.findElement(continueToCity).click();
+
+	public void closeCityBox() {
+		driver.findElement(cityCross).click();
 	}
-	
+
+	public void selectCity() {
+		driver.findElement(selectCityPin).click();
+	}
+
 	public void checkWithNoDisease() {
 		driver.findElement(noDiseaseCheckbox).click();
 		// if we tick none of these then automatically view plan will be clicked
 		// and result get displayed.
 	}
-	
+
 	public void checkWithAnyDisease() {
 		driver.findElement(anyDiseaseCheckBox).click();
-		// if we tick this then we have to click view plan button so use test case 
+		// if we tick this then we have to click view plan button so use test case
 		// accordingly.
 	}
-	
+
 	public void checkViewPlanButton() {
 		driver.findElement(viewPlan).click();
 	}
-	
+
+	public void clickSubmitButton() {
+		driver.findElement(submit).click();
+	}
+
 	public List<String> gettingPlans() {
+
 		List<String> plans = new ArrayList<String>();
-		// go with age 22 years then find plans with no disease check box and 
-		// retrieve items.
-		// data given
-		/* gender = female
-		 * name = Rani
-		 * number = 8888888888
-		 * person = Me
-		 * Age = 22
-		 * City = Hyderabad / Pincode = 500092
-		 * disease = none
-		 * 
-		 * Filter use karna ho to kar lena bhai mai ni kar raha or kar dena comment delete
-		 */
-		plans.add(driver.findElement(firstPlan).getText());
-		plans.add(driver.findElement(secondPlan).getText());
-		plans.add(driver.findElement(thirdPlan).getText());
+		planNameElements = driver.findElements(planName);
+
+		for (WebElement x : planNameElements) {
+			String temp = x.getText();
+			plans.add(temp);
+		}
+
 		return plans;
 	}
-	
-	
-	
-	
+
 }
---
+++ project/src/test/java/test/TestHealthInsurancePage.java
@@ -0,0 +1,114 @@
+package test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+//https://health.policybazaar.com/?pq=health0&new=1&utm_content=home_v12
+//https://health.policybazaar.com/?iscompareexp=1&utm_content=home_v12_compareexp
+import handler.BrowserHandler;
+import pages.HealthInsurancePage;
+
+public class TestHealthInsurancePage {
+
+	public static WebDriver driver = null;
+	public HealthInsurancePage hi = null;
+
+	@BeforeTest
+	public void setUpBrowser() throws UnknownHostException {
+		driver = BrowserHandler.getWebDriver();
+		hi = new HealthInsurancePage(driver);
+	}
+
+	@BeforeTest(dependsOnMethods = "setUpBrowser")
+	public void openPage() {
+		hi.openPolicyBazaar();
+	}
+
+	@AfterTest
+	public void tearDown() {
+		// driver.quit();
+	}
+
+	@Test(priority = 1)
+	public void clickHealthInsurance() {
+		String title = hi.openHealthInsurancePage();
+		Assert.assertEquals("PolicyBazaar Health Insurance: Get a 5 Lac health plan @Rs. 373/Month", title);
+	}
+
+	@Test(priority = 2)
+	public void clickSubmit() {
+		hi.clickSubmitButton();
+	}
+
+	@Test(priority = 3)
+	public void selectAgeYoung() {
+		hi.selectYoungestAge();
+	}
+
+	@Test(priority = 4)
+	public void selectAgeOld() {
+		hi.selectEldestAge();
+	}
+
+	@Test(priority = 5)
+	public void openCityPage() {
+		hi.clickSubmitButton();
+	}
+
+	@Test(priority = 6)
+	public void enterPinCode() {
+		hi.checkCityBoxFromCityPinCode();
+	}
+
+	@Test(priority = 7)
+	public void enterCity() {
+		hi.closeCityBox();
+		hi.checkCityBoxFromCityName();
+	}
+
+	@Test(priority = 8)
+	public void selectCity() {
+		hi.selectCity();
+	}
+
+	@Test(priority = 9)
+	public void selectGender() {
+		hi.selectGender();
+	}
+
+	@Test(priority = 10)
+	public void enterName() {
+		hi.writeName();
+	}
+
+	@Test(priority = 11)
+	public void enterNumber() {
+		hi.writeNumber();
+	}
+
+	@Test(priority = 12)
+	public void viewMedicalHistoryPage() {
+		hi.checkContinueForDetails();
+	}
+
+	@Test(priority = 13)
+	public void checkNoDisease() {
+		hi.checkWithNoDisease();
+	}
+
+	@Test(priority = 14)
+	public void getPlansInfo() {
+		List<String> list = hi.gettingPlans();
+
+		for (String x : list) {
+			System.out.println(x);
+		}
+	}
+
+}
